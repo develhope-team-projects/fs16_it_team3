c6dbc472b67fda18c1ac4e95821d50655160fdda
marco bruno <marco2000bruno@gmail.com>
2023-11-16 18:05:40 +0100

    footer completo

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,20 +1,10 @@
-import { CartBtn } from '../components/CartBtn'
-import { CustomButton } from '../components/CustomButton'
-import { CustomInput } from '../components/CustomInput'
+import { Footer } from "../components/Footer"
 
 function App() {
 
   return (
     <>
-      <CustomButton text="paga"/>
-      <CustomButton text="login"/>
-      <CustomButton text="register"/>
-      <CustomButton text="uomo"/>
-      <CustomButton text="donna"/>
-      <CustomButton text="sophora"/>
-      <CustomInput text="text"/>
-      <CustomInput type="password" text="pass"/>
-      <CartBtn />
+    <Footer/>
     </>
   )
 }
