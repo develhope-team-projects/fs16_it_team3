ddedd057fc546dde09b234ae21235fa7c9ee23d4
Sven <chanck.mds@gmail.com>
2023-11-29 16:25:19 +0100

    added use of cookies

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Uomo from './components/pages/Uomo'
-import Donna from './components/pages/Donna'
-import Scarpe from './components/pages/Scarpe'
-import { useState } from 'react'
-import { CartPage } from './components/CartPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Uomo from './components/pages/Uomo';
+import Donna from './components/pages/Donna';
+import Scarpe from './components/pages/Scarpe';
+import { useEffect, useState } from 'react';
+import { CartPage } from './components/CartPage';
+import Cookies from 'js-cookie';
 
 function App() {
+  const initialCart = Cookies.get('cartArray');
+  const [cart, setCart] = useState(initialCart ? JSON.parse(initialCart) : []);
 
-  const [cart, setCart] = useState([])
+  useEffect(() => {
+    Cookies.set('cartArray', JSON.stringify(cart));
+  }, [cart]);
+
   return (
     <>
-    
-     <BrowserRouter>
-     <Routes>
-      <Route path='/uomo' element={<Uomo cart={cart} setCart={setCart}/>}> </Route>
-      <Route path='/donna' element={<Donna cart={cart} setCart={setCart}/>}> </Route>
-      <Route path='/scarpe' element={<Scarpe cart={cart} setCart={setCart}/>}> </Route>
-      <Route path='/carello' element={<CartPage cart={cart} setCart={setCart}/>}> </Route>
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/uomo' element={<Uomo cart={cart} setCart={setCart} />}></Route>
+          <Route path='/donna' element={<Donna cart={cart} setCart={setCart} />}></Route>
+          <Route path='/scarpe' element={<Scarpe cart={cart} setCart={setCart} />}></Route>
+          <Route path='/carello' element={<CartPage cart={cart} setCart={setCart} />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
